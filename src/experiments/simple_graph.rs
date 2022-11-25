4480217759ba07c1f09b2451f9803f24ec3968b1
Paul Heinrich <paul.heinrich@outlook.com>
2022-11-25 15:02:40 +0100

    implemented validity checks

--- src/experiments/simple_graph.rs
+++ src/experiments/simple_graph.rs
@@ -6,12 +6,23 @@ pub struct Graph {
 
 impl Graph {
     fn is_valid(&self) -> bool {
-        let mut res: bool = true;
+        self.has_valid_vertices() && self.valid_edges()
+    }
+
+    fn has_valid_vertices(&self) -> bool {
+        only_unique_values(&self.vertices)
+    }
+
+    fn valid_edges(&self) -> bool {
+        let mut res = true;
         for edge in self.edges.iter() {
-            let list: Vec<&Edge> = self.edges.iter().filter(|e| (*e).id == edge.id).collect();
-            res = res && list.len() == 1;
+            res = res && self.valid_edge(edge);
         }
-        res
+        res && only_unique_values(&self.edges)
+    }
+
+    fn valid_edge(&self, edge: &Edge) -> bool {
+        self.vertices.iter().any(|v| v.id == edge.from)
     }
 
     fn traverse_path_sum_weights(&self, path: Vec<i32>) -> f32 {
@@ -19,17 +30,39 @@ impl Graph {
     }
 }
 
-#[derive(Debug)]
+trait Id {
+    fn get_id(&self) -> i32;
+}
+
+fn only_unique_values<T: Id>(list: &Vec<T>) -> bool {
+    let mut x: Vec<i32> = list.iter().map(|v| v.get_id()).collect();
+    x.dedup();
+    x.len() == list.len()
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Vertex {
     pub id: i32,
 }
 
-#[derive(Debug, Clone, Copy)]
+impl Id for Vertex {
+    fn get_id(&self) -> i32 {
+        self.id
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Edge {
     pub id: i32,
     pub from: i32,
     pub to: i32,
     pub weight: f32,
+}
+
+impl Id for Edge {
+    fn get_id(&self) -> i32 {
+        self.id
+    }
 }
 
 #[cfg(test)]
@@ -51,5 +84,16 @@ mod tests {
         println!("{:?}", graph);
         println!("First edge is {:?}", graph.edges.get(0));
         println!("Graph is valid: {:?}", graph.is_valid());
+        println!("Graph has valid vertices: {:?}", graph.has_valid_vertices());
+    }
+
+    #[test]
+    fn test_invalid_graph() {
+        let graph = Graph {
+            vertices: vec![Vertex { id: 1 }, Vertex { id: 1 }],
+            edges: vec![Edge { id: 1, from: 3, to: 1, weight: 2.0 }, Edge { id: 2, from: 1, to: 1, weight: 2.0 }],
+        };
+        assert_eq!(graph.has_valid_vertices(), false);
+        assert_eq!(graph.has_valid_vertices(), false);
     }
 }
